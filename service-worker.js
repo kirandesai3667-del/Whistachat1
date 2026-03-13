4c6b55c9b2cd7079afd428100e7237f79b5645e2
kirandesai3667-del <kiran.desai3667@gmail.com>
2026-03-13 23:29:24 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "whistachat-cache-v1";
+const urlsToCache = [
+  "/Whistachat1/",
+  "/Whistachat1/index.html"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
